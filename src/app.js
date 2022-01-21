f1041c33c8a4223f2b9cf7e88d47e568c602b2b0
Carlos Eduardo <cpwaldow@gmail.com>
2022-01-21 02:29:42 -0300

    colocando cada fetch em um arquivo separado

--- src/app.js
+++ src/app.js
@@ -1,15 +1,8 @@
-const fetchApiList = async () => {
-  const response = await fetch(
-    'https://pokeapi.co/api/v2/pokemon?limit=100&offset=0',
-  );
-  const data = await response.json();
-  return data;
-};
-
 const resultReturn = async () => {
   const { results } = await fetchApiList();
   results.forEach((element) => {
     createComponent(element.name);
+    fetchApiCard(element.name);
   });
 };
 
@@ -17,6 +10,7 @@ const createComponent = (nome) => {
   const listagem = document.querySelector('#listagem');
   const section = document.createElement('section');
   const link = document.createElement('a');
+  //const image = document.createElement('img');
 
   section.classList.add('pokecard');
 
