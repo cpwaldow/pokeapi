a15a84fc3896d9625750171ea2b77234801b122f
Carlos Eduardo <cpwaldow@gmail.com>
2022-01-22 17:10:13 -0300

    colocando estilo para formar uma listagem e criar card para cada pokemon

--- src/fetchApiList.js
+++ src/fetchApiList.js
@@ -1,6 +1,6 @@
 const fetchApiList = async () => {
   const response = await fetch(
-    'https://pokeapi.co/api/v2/pokemon?limit=100&offset=0',
+    'https://pokeapi.co/api/v2/pokemon?limit=200&offset=0',
   );
   const data = await response.json();
   return data;
